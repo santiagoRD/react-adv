45dd67dd6c7078d59b2f60bd9ecf129c3bb61244
santiago rios diaz <santi_diaz17@hotmail.com>
2021-11-28 10:16:24 -0500

    Basic Lazyload ready

--- src/01-lazyload/pages/LazyPage2.tsx
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export const LazyPage2 = () => {
-    return (
-        <h1>Lazy page 2</h1>
-    )
-}
+  return <h1>Lazy page 2</h1>;
+};
+
+export default LazyPage2;
